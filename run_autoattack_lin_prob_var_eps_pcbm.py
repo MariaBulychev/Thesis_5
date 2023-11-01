e8965e8863b56b4bd60ea5ab1f0272f326fe8317
Maria Bulychev <mbulychev@spartan-login1.hpc.unimelb.edu.au>
2023-11-01 18:13:10 +1100

    Before evaluating hpcbm 3

---
+++ run_autoattack_lin_prob_var_eps_pcbm.py
@@ -0,0 +1,82 @@
+import sys
+import clip
+import torch
+import torch.nn as nn
+import csv
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, Subset
+from autoattack import AutoAttack
+from torchvision.transforms import ToPILImage
+
+class ModelWrapper(nn.Module):
+    def __init__(self, classifier, clip_model, resolution):
+        super(ModelWrapper, self).__init__()
+        self.classifier = classifier
+        self.clip_model = clip_model
+        self.preprocess = transforms.Compose([
+            transforms.Resize(resolution, interpolation=transforms.InterpolationMode.BICUBIC),
+            transforms.CenterCrop(resolution),
+            transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
+        ])
+
+    def forward(self, images):
+        images = self.preprocess(images)
+        features = self.clip_model.encode_image(images)
+        outputs = self.classifier(features.float().to(device))
+        return outputs
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load('RN50', device)
+batch_size = 128
+
+print("load model")
+classifier = torch.load("/data/gpfs/projects/punim2103/new_attempt_4_classifier_model_full.pth", map_location=device)
+resolution = 224
+wrapped_model = ModelWrapper(classifier, model, resolution).to(device)
+wrapped_model.eval()
+
+print("load data")
+test_dataset = datasets.CIFAR10(root="./data", train=False, transform=transforms.ToTensor())
+test_subset = Subset(test_dataset, range(1000))
+test_loader = DataLoader(test_subset, batch_size=batch_size)
+
+epsilon = float(sys.argv[1])
+
+adversary = AutoAttack(wrapped_model, norm='Linf', eps=epsilon, version='standard', device=device)
+
+csv_path = f'/data/gpfs/projects/punim2103/csv/hybrid_pcbm_3_results_eps_{epsilon}.csv'
+batch = 0
+results = []
+
+with open(csv_path, 'w', newline='') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow(["Epsilon", "Initial Accuracy", "Robust Accuracy", "Max Perturbation"])  # Header
+
+    for images, labels in test_loader:
+        print("start attack for epsilon:", epsilon)
+        batch += 1
+        print("batch "+str(batch))
+        
+        images, labels = images.to(device), labels.to(device)
+
+        outputs = wrapped_model(images)
+        _, predicted = torch.max(outputs, 1)
+        initial_acc = (predicted == labels).sum().item() / images.shape[0]
+        print(f'Initial Accuracy for Batch {batch}: {100 * initial_acc:.2f}%')
+
+        x_adv, robust_accuracy, res = adversary.run_standard_evaluation(images, labels, bs=batch_size)
+
+        torch.save(x_adv, f'/data/gpfs/projects/punim2103/autoattack_results/hybrid_pcbm_3/eps_{epsilon}_batch_{batch}_adv.pt')
+
+        results.append([100 * initial_acc, 100* robust_accuracy, res.item()])
+        csv_writer.writerow([epsilon, 100 * initial_acc, 100 * robust_accuracy, res.item()])
+
+        print("done")
+        if batch * batch_size >= 1000:
+            break
+
+    # Calculate and write the mean values at the end of the csv
+    mean_values = [epsilon] + [sum(col)/len(col) for col in zip(*results)]
+    csv_writer.writerow([])
+    csv_writer.writerow(["Mean for Epsilon", "Mean Initial Accuracy", "Mean Robust Accuracy", "Mean Max Perturbation"])
+    csv_writer.writerow(mean_values)
